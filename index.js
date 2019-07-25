00fdc62f4bb486404d1991235237ed8e8ab45df5
Sergio Perez <sergio13prez@gmail.com>
2019-07-25 00:47:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div div div div.target');
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div div');
+}
+
+
+function increaseRankBy(number){
+  var ranks1 = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for(let i = 0; i < ranks1.length; i++){
+   ranks1[i].innerHTML = parseInt(ranks1[i].innerHTML)+ number;
+  }
+}
 
