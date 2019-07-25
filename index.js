c93e8be1185bf3bb73c6fb725d8637af0584f3b9
Sergio Perez <sergio13prez@gmail.com>
2019-07-25 00:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div div div div.target');
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div div');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelector('ul.ranked-list li');
+  
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}
+}
+
+console.log(increaseRankBy(2));
