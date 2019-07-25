e12a3036117972dd9ee7e05b41848e83f8ef7167
Sergio Perez <sergio13prez@gmail.com>
2019-07-25 00:39:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function deepestChild(){
   return document.getElementById('grand-node').querySelector('div div div div div');
 }
 
+var ranks1 = document.getElementById('app').querySelectorAll('ul.rankedList li');
+
 function increaseRankBy(number){
-  var ranks1 = document.getElementById('app').querySelectorAll('ul.rankedList li');
-  
   for(let i = 0; i < ranks1.length; i++){
    ranks1[i].innerHTML = parseInt(ranks1[i].innerHTML)+ number;
   }
